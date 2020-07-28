e112cd2cebc3bd33c4c671cd40a00b26985ab31a
zhangyi-he <jefferyhe@outlook.com>
2020-07-28 19:57:30 +0100

    Create RFUN.R

---
+++ ParamApprox/v1.0/RFUN.R
@@ -0,0 +1,86 @@
+#' @title Parametric approximations for the Wright-Fisher model of population dynamics under natural selection at two linked loci
+#' @author Zhangyi He, Wenyang Lyu and Feng Yu
+
+#' version 1.0
+
+#' R functions
+
+#install.packages("MASS")
+library("MASS")
+
+#install.packages("coda")
+library("coda")
+
+#install.packages("inline")
+library("inline")
+#install.packages("Rcpp")
+library("Rcpp")
+#install.packages("RcppArmadillo")
+library("RcppArmadillo")
+
+#install.packages("compiler")
+library("compiler")
+#enableJIT(1)
+
+# call C++ functions
+sourceCpp("./ParamApprox/v1.0/CFUN_2L.cpp")
+
+################################################################################
+
+#' Simulate the haplotype frequency trajectories according to the two-locus Wright-Fisher model with selection
+#' Parameter setting
+#' @param sel_cof the selection coefficients at loci A and B
+#' @param dom_par the dominance parameters at loci A and B
+#' @param rec_rat the recombination rate between loci A and B
+#' @param pop_siz the number of the diploid individuals in the population
+#' @param int_frq the initial haplotype frequencies of the population
+#' @param int_gen the first generation of the simulated haplotype frequency trajectories
+#' @param lst_gen the last generation of the simulated haplotype frequency trajectories
+
+#' Standard version
+simulateTLWFMS <- function(sel_cof, dom_par, rec_rat, pop_siz, int_frq, int_gen, lst_gen) {
+  sel_cof_A <- sel_cof[1]
+  sel_cof_B <- sel_cof[2]
+  dom_par_A <- dom_par[1]
+  dom_par_B <- dom_par[2]
+
+  frq_pth <- simulateTLWFMS_arma(sel_cof_A, dom_par_A, sel_cof_B, dom_par_B, rec_rat, pop_siz, int_frq, int_gen, lst_gen)
+
+  return(frq_pth)
+}
+#' Compiled version
+cmpsimulateTLWFMS <- cmpfun(simulateTLWFMS)
+
+########################################
+
+#' Simulate the haplotype frequency trajectories according to the two-locus Wright-Fisher diffusion with selection using the Euler-Maruyama method
+#' Parameter setting
+#' @param sel_cof the selection coefficients at loci A and B
+#' @param dom_par the dominance parameters at loci A and B
+#' @param rec_rat the recombination rate between loci A and B
+#' @param pop_siz the number of the diploid individuals in the population
+#' @param int_frq the initial haplotype frequencies of the population
+#' @param int_gen the first generation of the simulated haplotype frequency trajectories
+#' @param lst_gen the last generation of the simulated haplotype frequency trajectories
+#' @param ptn_num the number of subintervals divided per generation in the Euler-Maruyama method
+#' @param data_augmentation = TRUE/FALSE (return the simulated sample trajectory with data augmentation or not)
+
+#' Standard version
+simulateTLWFDS <- function(sel_cof, dom_par, rec_rat, pop_siz, int_frq, int_gen, lst_gen, ptn_num, data_augmentation = TRUE) {
+  sel_cof_A <- sel_cof[1]
+  sel_cof_B <- sel_cof[2]
+  dom_par_A <- dom_par[1]
+  dom_par_B <- dom_par[2]
+
+  frq_pth <- simulateTLWFDS_arma(sel_cof_A, dom_par_A, sel_cof_B, dom_par_B, rec_rat, pop_siz, int_frq, int_gen, lst_gen, ptn_num)
+
+  if (data_augmentation == FALSE) {
+    return(frq_pth[, (0:(lst_gen - int_gen)) * ptn_num + 1])
+  } else {
+    return(frq_pth)
+  }
+}
+#' Compiled version
+cmpsimulateTLWFDS <- cmpfun(simulateTLWFDS)
+
+################################################################################
