0fcbf16a1470ef48a40ee11ed15de50cb1a2a4ad
zhangyi-he <jefferyhe@outlook.com>
2020-07-28 23:47:36 +0100

    Update RFUN.R

--- DiffusApprox/v1.0/RFUN.R
+++ DiffusApprox/v1.0/RFUN.R
@@ -131,4 +131,124 @@ simulateTLWFDS <- function(sel_cof, dom_par, rec_rat, pop_siz, int_frq, int_gen,
 #' Compiled version
 cmpsimulateTLWFDS <- cmpfun(simulateTLWFDS)
 
+########################################
+
+#' Simulate the haplotype frequency trajectories according to the two-locus Wright-Fisher diffusion with selection using the Euler-Maruyama method
+#' Parameter setting
+#' @param sel_cof the selection coefficients at loci A and B
+#' @param dom_par the dominance parameters at loci A and B
+#' @param rec_rat the recombination rate between loci A and B
+#' @param pop_siz the number of the diploid individuals in the population
+#' @param int_frq the initial haplotype frequencies of the population
+#' @param int_gen the first generation of the simulated haplotype frequency trajectories
+#' @param lst_gen the last generation of the simulated haplotype frequency trajectories
+#' @param ptn_num the number of subintervals divided per generation in the Euler-Maruyama method
+#' @param data_augmentation = TRUE/FALSE (return the simulated sample trajectory with data augmentation or not)
+
+#' Standard version
+simulateTLWFDS <- function(sel_cof, dom_par, rec_rat, pop_siz, int_frq, int_gen, lst_gen, ptn_num, data_augmentation = TRUE) {
+  sel_cof_A <- sel_cof[1]
+  sel_cof_B <- sel_cof[2]
+  dom_par_A <- dom_par[1]
+  dom_par_B <- dom_par[2]
+
+  frq_pth <- simulateTLWFDS_arma(sel_cof_A, dom_par_A, sel_cof_B, dom_par_B, rec_rat, pop_siz, int_frq, int_gen, lst_gen, ptn_num)
+
+  if (data_augmentation == FALSE) {
+    return(frq_pth[, (0:(lst_gen - int_gen)) * ptn_num + 1])
+  } else {
+    return(frq_pth)
+  }
+}
+#' Compiled version
+cmpsimulateTLWFDS <- cmpfun(simulateTLWFDS)
+
+########################################
+
+#' Simulate the haplotype frequency trajectories according to the guided process using the Euler-Maruyama method
+#' Parameter setting
+#' @param sel_cof the selection coefficients at loci A and B
+#' @param dom_par the dominance parameters at loci A and B
+#' @param rec_rat the recombination rate between loci A and B
+#' @param pop_siz the number of the diploid individuals in the population
+#' @param int_frq the initial haplotype frequencies of the population
+#' @param int_gen the first generation of the simulated haplotype frequency trajectories
+#' @param lst_gen the last generation of the simulated haplotype frequency trajectories
+#' @param ptn_num the number of subintervals divided per generation in the Euler-Maruyama method
+#' @param data_augmentation = TRUE/FALSE (return the simulated sample trajectory with data augmentation or not)
+#' @param guided_proc the guided process (Fearnhead (2008) or He et al. (2020))
+#' @param modification = TRUE/FALSE
+#' @param rho 
+
+#' Standard version
+simulateGuidedProc <- function(sel_cof, dom_par, rec_rat, pop_siz, int_frq, int_gen, lst_gen, ptn_num, data_augmentation = TRUE, guided_proc, modification = TRUE, ...) {
+  sel_cof_A <- sel_cof[1]
+  sel_cof_B <- sel_cof[2]
+  dom_par_A <- dom_par[1]
+  dom_par_B <- dom_par[2]
+
+  frq_pth <- simulateTLWFDS_arma(sel_cof_A, dom_par_A, sel_cof_B, dom_par_B, rec_rat, pop_siz, int_frq, int_gen, lst_gen, ptn_num)
+
+  if (data_augmentation == FALSE) {
+    return(frq_pth[, (0:(lst_gen - int_gen)) * ptn_num + 1])
+  } else {
+    return(frq_pth)
+  }
+}
+#' Compiled version
+cmpsimulateGuidedProc <- cmpfun(simulateGuidedProc)
+
+########################################
+
+#' Approximate the transition probability density of the two-locus Wright-Fisher model with selection using Monte Carlo integration
+#' Parameter setting
+#' @param sel_cof the selection coefficients at loci A and B
+#' @param dom_par the dominance parameters at loci A and B
+#' @param rec_rat the recombination rate between loci A and B
+#' @param pop_siz the number of the diploid individuals in the population
+#' @param int_frq the initial haplotype frequencies of the population
+#' @param int_gen the first generation of the simulated haplotype frequency trajectories
+#' @param lst_gen the last generation of the simulated haplotype frequency trajectories
+#' @param ptn_num the number of subintervals divided per generation in the Euler-Maruyama method
+#' @param sim_num the number of Monte Carlo simulations
+
+
+
+########################################
+
+#' Calculate the importance weight
+#' Parameter setting
+#' @param sel_cof the selection coefficients at loci A and B
+#' @param dom_par the dominance parameters at loci A and B
+#' @param rec_rat the recombination rate between loci A and B
+#' @param pop_siz the number of the diploid individuals in the population
+#' @param int_frq the initial haplotype frequencies of the population
+#' @param int_gen the first generation of the simulated haplotype frequency trajectories
+#' @param lst_gen the last generation of the simulated haplotype frequency trajectories
+#' @param ptn_num the number of subintervals divided per generation in the Euler-Maruyama method
+#' @param guided_proc the guided process (Fearnhead (2008) or He et al. (2020))
+#' @param modification = TRUE/FALSE
+#' @param rho 
+
+
+
+########################################
+
+#' Approximate the transition probability density of the two-locus Wright-Fisher model with selection using Monte Carlo integration with importance sampling
+#' Parameter setting
+#' @param sel_cof the selection coefficients at loci A and B
+#' @param dom_par the dominance parameters at loci A and B
+#' @param rec_rat the recombination rate between loci A and B
+#' @param pop_siz the number of the diploid individuals in the population
+#' @param int_frq the initial haplotype frequencies of the population
+#' @param int_gen the first generation of the simulated haplotype frequency trajectories
+#' @param lst_gen the last generation of the simulated haplotype frequency trajectories
+#' @param ptn_num the number of subintervals divided per generation in the Euler-Maruyama method
+#' @param sim_num the number of Monte Carlo simulations
+#' @param guided_proc the guided process (Fearnhead (2008) or He et al. (2020))
+#' @param modification = TRUE/FALSE
+#' @param rho 
+
+
+
 ################################################################################
