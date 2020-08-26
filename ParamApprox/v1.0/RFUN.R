ade0eaefa22be97b3e67e059a245cb1c65eb3268
zhangyi-he <jefferyhe@outlook.com>
2020-08-26 22:53:57 +0100

    Update RFUN.R

--- ParamApprox/v1.0/RFUN.R
+++ ParamApprox/v1.0/RFUN.R
@@ -44,7 +44,9 @@ simulateWFM <- function(sel_cof, dom_par, rec_rat, pop_siz, int_frq, int_gen, ls
   dom_par_A <- dom_par[1]
   dom_par_B <- dom_par[2]
 
-  frq_pth <- simulateWFM_arma(sel_cof_A, dom_par_A, sel_cof_B, dom_par_B, rec_rat, pop_siz, int_frq, int_gen, lst_gen)
+  fts_mat <- calculateFitnessMat_arma(sel_cof_A, dom_par_A, sel_cof_B, dom_par_B)
+
+  frq_pth <- simulateWFM_arma(fts_mat, rec_rat, pop_siz, int_frq, int_gen, lst_gen)
 
   return(frq_pth)
 }
@@ -63,10 +65,10 @@ cmpsimulateWFM <- cmpfun(simulateWFM)
 #' @param int_gen the first generation of the simulated haplotype frequency trajectories
 #' @param lst_gen the last generation of the simulated haplotype frequency trajectories
 #' @param ptn_num the number of subintervals divided per generation in the Euler-Maruyama method
-#' @param data_augmentation = TRUE/FALSE (return the simulated sample trajectory with data augmentation or not)
+#' @param dat_aug = TRUE/FALSE (return the simulated sample trajectory with data augmentation or not)
 
 #' Standard version
-simulateDiffusApprox <- function(sel_cof, dom_par, rec_rat, pop_siz, int_frq, int_gen, lst_gen, ptn_num, data_augmentation = TRUE) {
+simulateDiffusApprox <- function(sel_cof, dom_par, rec_rat, pop_siz, int_frq, int_gen, lst_gen, ptn_num, dat_aug = TRUE) {
   sel_cof_A <- sel_cof[1]
   sel_cof_B <- sel_cof[2]
   dom_par_A <- dom_par[1]
@@ -74,7 +76,7 @@ simulateDiffusApprox <- function(sel_cof, dom_par, rec_rat, pop_siz, int_frq, in
 
   frq_pth <- simulateDiffusApprox_arma(sel_cof_A, dom_par_A, sel_cof_B, dom_par_B, rec_rat, pop_siz, int_frq, int_gen, lst_gen, ptn_num)
 
-  if (data_augmentation == FALSE) {
+  if (dat_aug == FALSE) {
     return(frq_pth[, (0:(lst_gen - int_gen)) * ptn_num + 1])
   } else {
     return(frq_pth)
@@ -94,9 +96,21 @@ cmpsimulateDiffusApprox <- cmpfun(simulateDiffusApprox)
 #' @param int_frq the initial haplotype frequencies of the population
 #' @param int_gen the first generation of the simulated haplotype frequency trajectories
 #' @param lst_gen the last generation of the simulated haplotype frequency trajectories
-#' @param moment_approx the moment approximation (Monte Carlo, Lacerda & Seoighe (2014), Terhorst et al. (2015) or Paris et al. (2019))
+#' @param mnt_apx the moment approximation (Monte Carlo, Lacerda & Seoighe (2014), Terhorst et al. (2015) or Paris et al. (2019))
+#' @param sim_num the number of the samples in Monte Carlo simulation
+
+#' Standard version
+approximateMoment <- function(sel_cof, dom_par, rec_rat, pop_siz, int_frq, int_gen, lst_gen, mnt_apx, ...) {
+  sel_cof_A <- sel_cof[1]
+  sel_cof_B <- sel_cof[2]
+  dom_par_A <- dom_par[1]
+  dom_par_B <- dom_par[2]
 
 
+
+}
+#' Compiled version
+cmpapproximateMoment <- cmpfun(approximateMoment)
 
 ########################################
 
@@ -109,13 +123,26 @@ cmpsimulateDiffusApprox <- cmpfun(simulateDiffusApprox)
 #' @param int_frq the initial haplotype frequencies of the population
 #' @param int_gen the first generation of the simulated haplotype frequency trajectories
 #' @param lst_gen the last generation of the simulated haplotype frequency trajectories
-#' @param moment_approx the moment approximation (Monte Carlo, Lacerda & Seoighe (2014), Terhorst et al. (2015) or Paris et al. (2019))
+#' @param mnt_apx the moment approximation (Monte Carlo, Lacerda & Seoighe (2014), Terhorst et al. (2015) or Paris et al. (2019))
+#' @param sim_num the number of the samples in Monte Carlo simulation
+
+#' Standard version
+approximateWFM_Norm <- function(sel_cof, dom_par, rec_rat, pop_siz, int_frq, int_gen, lst_gen, mnt_apx, ...) {
+  sel_cof_A <- sel_cof[1]
+  sel_cof_B <- sel_cof[2]
+  dom_par_A <- dom_par[1]
+  dom_par_B <- dom_par[2]
 
 
+  # return the parameters of the normal approximation and the corresponding first two moments
+
+}
+#' Compiled version
+cmpapproximateWFM_Norm <- cmpfun(approximateWFM_Norm)
 
 ########################################
 
-#' Approximate the two-locus Wright-Fisher model with selection using the logitic normal
+#' Simulate the two-locus Wright-Fisher model with selection using the logistic normal
 #' Parameter setting
 #' @param sel_cof the selection coefficients at loci A and B
 #' @param dom_par the dominance parameters at loci A and B
@@ -124,9 +151,78 @@ cmpsimulateDiffusApprox <- cmpfun(simulateDiffusApprox)
 #' @param int_frq the initial haplotype frequencies of the population
 #' @param int_gen the first generation of the simulated haplotype frequency trajectories
 #' @param lst_gen the last generation of the simulated haplotype frequency trajectories
-#' @param moment_approx the moment approximation (Monte Carlo, Lacerda & Seoighe (2014), Terhorst et al. (2015) or Paris et al. (2019))
+#' @param sim_num the number of the samples in Monte Carlo simulation
+#' @param mnt_apx the moment approximation (Monte Carlo, Lacerda & Seoighe (2014), Terhorst et al. (2015) or Paris et al. (2019))
+#' @param smp_siz the number of the Monte Carlo samples
+
+#' Standard version
+simulateWFM_LogisticNorm <- function(sel_cof, dom_par, rec_rat, pop_siz, int_frq, int_gen, lst_gen, sim_num, mnt_apx, ...) {
+  sel_cof_A <- sel_cof[1]
+  sel_cof_B <- sel_cof[2]
+  dom_par_A <- dom_par[1]
+  dom_par_B <- dom_par[2]
 
 
+
+}
+#' Compiled version
+cmpsimulateWFM_LogisticNorm <- cmpfun(simulateWFM_LogisticNorm)
+
+########################################
+
+#' Approximate the two-locus Wright-Fisher model with selection using the logistic normal
+#' Parameter setting
+#' @param sel_cof the selection coefficients at loci A and B
+#' @param dom_par the dominance parameters at loci A and B
+#' @param rec_rat the recombination rate between loci A and B
+#' @param pop_siz the number of the diploid individuals in the population
+#' @param int_frq the initial haplotype frequencies of the population
+#' @param int_gen the first generation of the simulated haplotype frequency trajectories
+#' @param lst_gen the last generation of the simulated haplotype frequency trajectories
+#' @param mnt_apx the moment approximation (Monte Carlo, Lacerda & Seoighe (2014), Terhorst et al. (2015) or Paris et al. (2019))
+#' @param smp_siz the number of the Monte Carlo samples
+
+#' Standard version
+approximateWFM_LogisticNorm <- function(sel_cof, dom_par, rec_rat, pop_siz, int_frq, int_gen, lst_gen, mnt_apx, ...) {
+  sel_cof_A <- sel_cof[1]
+  sel_cof_B <- sel_cof[2]
+  dom_par_A <- dom_par[1]
+  dom_par_B <- dom_par[2]
+
+
+  # return the parameters of the logistic normal approximation and the corresponding first two moments
+
+}
+#' Compiled version
+cmpapproximateWFM_LogisticNorm <- cmpfun(approximateWFM_LogisticNorm)
+
+########################################
+
+#' Simulate the two-locus Wright-Fisher model with selection using the logistic normal
+#' Parameter setting
+#' @param sel_cof the selection coefficients at loci A and B
+#' @param dom_par the dominance parameters at loci A and B
+#' @param rec_rat the recombination rate between loci A and B
+#' @param pop_siz the number of the diploid individuals in the population
+#' @param int_frq the initial haplotype frequencies of the population
+#' @param int_gen the first generation of the simulated haplotype frequency trajectories
+#' @param lst_gen the last generation of the simulated haplotype frequency trajectories
+#' @param sim_num the number of the samples in Monte Carlo simulation
+#' @param mnt_apx the moment approximation (Monte Carlo, Lacerda & Seoighe (2014), Terhorst et al. (2015) or Paris et al. (2019))
+#' @param smp_siz the number of the Monte Carlo samples
+
+#' Standard version
+simulateWFM_LogisticNorm <- function(sel_cof, dom_par, rec_rat, pop_siz, int_frq, int_gen, lst_gen, sim_num, mnt_apx, ...) {
+  sel_cof_A <- sel_cof[1]
+  sel_cof_B <- sel_cof[2]
+  dom_par_A <- dom_par[1]
+  dom_par_B <- dom_par[2]
+
+
+
+}
+#' Compiled version
+cmpsimulateWFM_LogisticNorm <- cmpfun(simulateWFM_LogisticNorm)
 
 ########################################
 
@@ -139,8 +235,49 @@ cmpsimulateDiffusApprox <- cmpfun(simulateDiffusApprox)
 #' @param int_frq the initial haplotype frequencies of the population
 #' @param int_gen the first generation of the simulated haplotype frequency trajectories
 #' @param lst_gen the last generation of the simulated haplotype frequency trajectories
-#' @param moment_approx the moment approximation (Monte Carlo, Lacerda & Seoighe (2014), Terhorst et al. (2015) or Paris et al. (2019))
+#' @param mnt_apx the moment approximation (Monte Carlo, Lacerda & Seoighe (2014), Terhorst et al. (2015) or Paris et al. (2019))
+#' @param smp_siz the number of the Monte Carlo samples
+
+#' Standard version
+approximateWFM_HierarchicalBeta <- function(sel_cof, dom_par, rec_rat, pop_siz, int_frq, int_gen, lst_gen, mnt_apx, ...) {
+  sel_cof_A <- sel_cof[1]
+  sel_cof_B <- sel_cof[2]
+  dom_par_A <- dom_par[1]
+  dom_par_B <- dom_par[2]
 
 
+  # return the parameters of the hierarchical beta approximation and the corresponding first two moments
+
+}
+#' Compiled version
+cmpapproximateWFM_HierarchicalBeta <- cmpfun(approximateWFM_HierarchicalBeta)
+
+########################################
+
+#' Simulate the two-locus Wright-Fisher model with selection using the hierarchical beta
+#' Parameter setting
+#' @param sel_cof the selection coefficients at loci A and B
+#' @param dom_par the dominance parameters at loci A and B
+#' @param rec_rat the recombination rate between loci A and B
+#' @param pop_siz the number of the diploid individuals in the population
+#' @param int_frq the initial haplotype frequencies of the population
+#' @param int_gen the first generation of the simulated haplotype frequency trajectories
+#' @param lst_gen the last generation of the simulated haplotype frequency trajectories
+#' @param sim_num the number of the samples in Monte Carlo simulation
+#' @param mnt_apx the moment approximation (Monte Carlo, Lacerda & Seoighe (2014), Terhorst et al. (2015) or Paris et al. (2019))
+#' @param smp_siz the number of the Monte Carlo samples
+
+#' Standard version
+simulateWFM_HierarchicalBeta <- function(sel_cof, dom_par, rec_rat, pop_siz, int_frq, int_gen, lst_gen, sim_num, mnt_apx, ...) {
+  sel_cof_A <- sel_cof[1]
+  sel_cof_B <- sel_cof[2]
+  dom_par_A <- dom_par[1]
+  dom_par_B <- dom_par[2]
+
+
+
+}
+#' Compiled version
+cmpsimulateWFM_HierarchicalBeta <- cmpfun(simulateWFM_HierarchicalBeta)
 
 ################################################################################
