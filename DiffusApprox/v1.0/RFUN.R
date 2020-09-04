65a3cf544855aa2e4ab6cfd29cc18b99e8c242b9
zhangyi-he <jefferyhe@outlook.com>
2020-09-04 16:18:53 +0100

    Update RFUN.R

--- DiffusApprox/v1.0/RFUN.R
+++ DiffusApprox/v1.0/RFUN.R
@@ -53,7 +53,7 @@ cmpsimulateWFM_1L <- cmpfun(simulateWFM_1L)
 #' @param int_frq the initial mutant allele frequency of the population
 #' @param int_gen the first generation of the simulated mutant allele frequency trajectory
 #' @param lst_gen the last generation of the simulated mutant allele frequency trajectory
-#' @param ptn_num the number of subintervals divided per generation in the Euler-Maruyama method
+#' @param ptn_num the number of the subintervals divided per generation in the Euler-Maruyama method
 #' @param dat_aug = TRUE/FALSE (return the simulated sample trajectory with data augmentation or not)
 
 #' Standard version
@@ -69,6 +69,34 @@ simulateWFD_1L <- function(sel_cof, dom_par, pop_siz, int_frq, int_gen, lst_gen,
 }
 #' Compiled version
 cmpsimulateWFD_1L <- cmpfun(simulateWFD_1L)
+
+########################################
+
+#' Generate the sample mutant allele frequency trajectories according to the one-locus Wright-Fisher model/diffusion with selection
+#' Parameter setting
+#' @param model = WFM/WFD (return the sample mutant allele frequency trajectories according to the one-locus Wright-Fisher model/diffusion with selection)
+#' @param sel_cof the selection coefficient
+#' @param dom_par the dominance parameter
+#' @param pop_siz the number of the diploid individuals in the population
+#' @param int_frq the initial mutant allele frequency of the population
+#' @param int_gen the first generation of the simulated mutant allele frequency trajectory
+#' @param lst_gen the last generation of the simulated mutant allele frequency trajectory
+#' @param sim_num the number of the samples in Monte Carlo simulation
+#' @param ptn_num the number of the subintervals divided per generation in the Euler-Maruyama method
+
+#' Standard version
+generateSampleTraj_1L <- function(model, sel_cof, dom_par, pop_siz, int_frq, int_gen, lst_gen, sim_num, ...) {
+  if (model == "WFM") {
+    smp_pth <- generateSample_WFM_1L_arma(sel_cof, dom_par, pop_siz, int_frq, int_gen, lst_gen, sim_num)
+  } else {
+    smp_pth <- generateSample_WFD_1L_arma(sel_cof, dom_par, pop_siz, int_frq, int_gen, lst_gen, ptn_num, sim_num)
+    smp_pth <- smp_pth[, (0:(lst_gen - int_gen)) * ptn_num + 1]
+  }
+
+  return(smp_pth)
+}
+#' Compiled version
+cmpgenerateSampleTraj_1L <- cmpfun(generateSampleTraj_1L)
 
 ########################################
 
@@ -109,7 +137,7 @@ cmpsimulateWFM_2L <- cmpfun(simulateWFM_2L)
 #' @param int_frq the initial haplotype frequencies of the population
 #' @param int_gen the first generation of the simulated haplotype frequency trajectories
 #' @param lst_gen the last generation of the simulated haplotype frequency trajectories
-#' @param ptn_num the number of subintervals divided per generation in the Euler-Maruyama method
+#' @param ptn_num the number of the subintervals divided per generation in the Euler-Maruyama method
 #' @param dat_aug = TRUE/FALSE (return the simulated sample trajectory with data augmentation or not)
 
 #' Standard version
@@ -132,9 +160,9 @@ cmpsimulateWFD_2L <- cmpfun(simulateWFD_2L)
 
 ########################################
 
-#' Compute the empirical cumulative distribution function for the Wright-Fisher model or the Wright-Fisher diffusion
+#' Generate the sample haplotype frequency trajectories according to the two-locus Wright-Fisher model/diffusion with selection
 #' Parameter setting
-#' @param model = WFM/WFD (return the empirical cumulative distribution function for the Wright-Fisher model or the Wright-Fisher diffusion)
+#' @param model = WFM/WFD (return the sample haplotype frequency trajectories according to the two-locus Wright-Fisher model/diffusion with selection)
 #' @param sel_cof the selection coefficients at loci A and B
 #' @param dom_par the dominance parameters at loci A and B
 #' @param rec_rat the recombination rate between loci A and B
@@ -143,13 +171,48 @@ cmpsimulateWFD_2L <- cmpfun(simulateWFD_2L)
 #' @param int_gen the first generation of the simulated haplotype frequency trajectories
 #' @param lst_gen the last generation of the simulated haplotype frequency trajectories
 #' @param sim_num the number of the samples in Monte Carlo simulation
-#' @param ptn_num the number of subintervals divided per generation in the Euler-Maruyama method
+#' @param ptn_num the number of the subintervals divided per generation in the Euler-Maruyama method
+
+#' Standard version
+generateSampleTraj_2L <- function(model, sel_cof, dom_par, rec_rat, pop_siz, int_frq, int_gen, lst_gen, sim_num, ...) {
+  sel_cof_A <- sel_cof[1]
+  sel_cof_B <- sel_cof[2]
+  dom_par_A <- dom_par[1]
+  dom_par_B <- dom_par[2]
+
+  if (model == "WFM") {
+    fts_mat <- calculateFitnessMat_2L_arma(sel_cof_A, dom_par_A, sel_cof_B, dom_par_B)
+    smp_pth <- generateSample_WFM_2L_arma(fts_mat, rec_rat, pop_siz, int_frq, int_gen, lst_gen, sim_num)
+  } else {
+    smp_pth <- generateSample_WFD_2L_arma(sel_cof_A, dom_par_A, sel_cof_B, dom_par_B, rec_rat, pop_siz, int_frq, int_gen, lst_gen, ptn_num, sim_num)
+    smp_pth <- smp_pth[, , (0:(lst_gen - int_gen)) * ptn_num + 1]
+  }
+
+  return(smp_pth)
+}
+#' Compiled version
+cmpgenerateSampleTraj_2L <- cmpfun(generateSampleTraj_2L)
+
+########################################
+
+#' Compute the empirical cumulative distribution function for the two-locus Wright-Fisher model/diffusion with selection
+#' Parameter setting
+#' @param model = WFM/WFD (return the empirical cumulative distribution function for the two-locus Wright-Fisher model/diffusion with selection)
+#' @param sel_cof the selection coefficients at loci A and B
+#' @param dom_par the dominance parameters at loci A and B
+#' @param rec_rat the recombination rate between loci A and B
+#' @param pop_siz the number of the diploid individuals in the population
+#' @param int_frq the initial haplotype frequencies of the population
+#' @param int_gen the first generation of the simulated haplotype frequency trajectories
+#' @param lst_gen the last generation of the simulated haplotype frequency trajectories
+#' @param sim_num the number of the samples in Monte Carlo simulation
+#' @param ptn_num the number of the subintervals divided per generation in the Euler-Maruyama method
 
 
 
 ########################################
 
-#' Calculate the distance between the empirical cumulative distribution functions for the Wright-Fisher model or the Wright-Fisher diffusion
+#' Calculate the distance between the empirical cumulative distribution functions for the two-locus Wright-Fisher model/diffusion with selection
 #' Parameter setting
 #' @param sel_cof the selection coefficients at loci A and B
 #' @param dom_par the dominance parameters at loci A and B
@@ -159,7 +222,7 @@ cmpsimulateWFD_2L <- cmpfun(simulateWFD_2L)
 #' @param int_gen the first generation of the simulated haplotype frequency trajectories
 #' @param lst_gen the last generation of the simulated haplotype frequency trajectories
 #' @param sim_num the number of the samples in Monte Carlo simulation
-#' @param ptn_num the number of subintervals divided per generation in the Euler-Maruyama method
+#' @param ptn_num the number of the subintervals divided per generation in the Euler-Maruyama method
 
   sel_cof_A <- sel_cof[1]
   sel_cof_B <- sel_cof[2]
@@ -192,7 +255,7 @@ cmpsimulateWFD_2L <- cmpfun(simulateWFD_2L)
 #' @param int_frq the initial haplotype frequencies of the population
 #' @param int_gen the first generation of the simulated haplotype frequency trajectories
 #' @param lst_gen the last generation of the simulated haplotype frequency trajectories
-#' @param ptn_num the number of subintervals divided per generation in the Euler-Maruyama method
+#' @param ptn_num the number of the subintervals divided per generation in the Euler-Maruyama method
 #' @param dat_aug = TRUE/FALSE (return the simulated sample trajectory with data augmentation or not)
 #' @param guided_proc the guided process (Fearnhead (2008) or He et al. (2020))
 #' @param modification = TRUE/FALSE
