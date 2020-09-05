352bef3fc97f377cf160870b86b62de9790a4e3d
zhangyi-he <jefferyhe@outlook.com>
2020-09-05 13:27:19 +0100

    Update CFUN.cpp

--- DiffusApprox/v1.0/CFUN.cpp
+++ DiffusApprox/v1.0/CFUN.cpp
@@ -266,7 +266,9 @@ arma::dmat generateSample_WFD_1L_arma(const double& sel_cof, const double& dom_p
 
   arma::dmat frq_smp = arma::zeros<arma::dmat>(sim_num, arma::uword(lst_gen - int_gen) * ptn_num + 1);
   for (arma::uword i = 0; i < sim_num; i++) {
-    frq_smp.row(i) = simulateWFD_1L_arma(sel_cof, dom_par, pop_siz, int_frq, int_gen, lst_gen, ptn_num);
+    arma::dmat frq_pth = simulateWFD_1L_arma(sel_cof, dom_par, pop_siz, int_frq, int_gen, lst_gen, ptn_num);
+    arma::ucolvec sel_gen = arma::linspace<arma::ucolvec>(0, arma::uword(lst_gen - int_gen), arma::uword(lst_gen - int_gen) + 1);
+    frq_smp.row(i) = frq_pth.cols(sel_gen);
   }
 
   return frq_smp;
@@ -292,9 +294,11 @@ arma::dcube generateSample_WFD_2L_arma(const double& sel_cof_A, const double& do
   // ensure RNG gets set/reset
   RNGScope scope;
 
-  arma::dcube frq_smp = arma::zeros<arma::dcube>(sim_num, 4, arma::uword(lst_gen - int_gen) * ptn_num + 1);
+  arma::dcube frq_smp = arma::zeros<arma::dcube>(sim_num, 4, arma::uword(lst_gen - int_gen) + 1);
   for (arma::uword i = 0; i < sim_num; i++) {
-    frq_smp.row(i) = simulateWFD_2L_arma(sel_cof_A, dom_par_A, sel_cof_B, dom_par_B, rec_rat, pop_siz, int_frq, int_gen, lst_gen, ptn_num);
+    arma::dmat frq_pth = simulateWFD_2L_arma(sel_cof_A, dom_par_A, sel_cof_B, dom_par_B, rec_rat, pop_siz, int_frq, int_gen, lst_gen, ptn_num);
+    arma::ucolvec sel_gen = arma::linspace<arma::ucolvec>(0, arma::uword(lst_gen - int_gen), arma::uword(lst_gen - int_gen) + 1);
+    frq_smp.row(i) = frq_pth.cols(sel_gen);
   }
 
   return frq_smp;
