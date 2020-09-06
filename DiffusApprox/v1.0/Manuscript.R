78d44195eaa9e7da2ad96a847c968373ad81d5c4
zhangyi-he <jefferyhe@outlook.com>
2020-09-06 21:30:01 +0100

    Update Manuscript.R

--- DiffusApprox/v1.0/Manuscript.R
+++ DiffusApprox/v1.0/Manuscript.R
@@ -54,6 +54,60 @@ setwd("~/Dropbox/Jeffery He/iResearch/Publications/2012/HE2020-WFM-2L-DiffusAppr
 
 #' @section Section 3.2. Performance evaluation
 
+sel_cof <- c(1e-02, 5e-03)
+dom_par <- c(5e-01, 5e-01)
+rec_rat <- 1e-03
+pop_siz <- 5e+03
+int_frq <- c(2e-01, 2e-01, 2e-01, 4e-01)
+int_gen <- 0
+lst_gen <- 100
+sim_num <- 1e+05
+
+dist <- array(NA, dim = c(3, 4, lst_gen - int_gen + 1))
+model <- "WFM"
+smp_WFM <- cmpgenerateSampleTraj_2L(model, sel_cof, dom_par, rec_rat, pop_siz, int_frq, int_gen, lst_gen, sim_num)
+model <- "WFD"
+ptn_num <- 1e+00
+smp_WFD <- cmpgenerateSampleTraj_2L(model, sel_cof, dom_par, rec_rat, pop_siz, int_frq, int_gen, lst_gen, sim_num, ptn_num)
+grd_num <- 2 * pop_siz
+dist[1, , ] <- cmpcalculateHellingerDist(smp_WFM, smp_WFD, grd_num)
+model <- "WFD"
+ptn_num <- 1e+01
+smp_WFD <- cmpgenerateSampleTraj_2L(model, sel_cof, dom_par, rec_rat, pop_siz, int_frq, int_gen, lst_gen, sim_num, ptn_num)
+grd_num <- 2 * pop_siz
+dist[2, , ] <- cmpcalculateHellingerDist(smp_WFM, smp_WFD, grd_num)
+model <- "WFD"
+ptn_num <- 1e+02
+smp_WFD <- cmpgenerateSampleTraj_2L(model, sel_cof, dom_par, rec_rat, pop_siz, int_frq, int_gen, lst_gen, sim_num, ptn_num)
+grd_num <- 2 * pop_siz
+dist[3, , ] <- cmpcalculateHellingerDist(smp_WFM, smp_WFD, grd_num)
+
+plot(int_gen:lst_gen, dist[1, 4, ], type = "l")
+lines(int_gen:lst_gen, dist[2, 4, ])
+lines(int_gen:lst_gen, dist[3, 4, ])
+
+sel_cof <- c(1e-02, 5e-03)
+dom_par <- c(5e-01, 5e-01)
+rec_rat <- 1e-03
+pop_siz <- 5e+03
+int_frq <- c(5e-03, 5e-03, 9e-02, 9e-01)
+int_gen <- 0
+lst_gen <- 500
+sim_num <- 5e+05
+ptn_num <- c(1e+00, 1e+01, 1e+02)
+
+dist <- array(NA, dim = c(3, 4, lst_gen - int_gen + 1))
+for (i in 1:3) {
+  model <- "WFM"
+  smp_WFM <- cmpgenerateSampleTraj_2L(model, sel_cof, dom_par, rec_rat, pop_siz, int_frq, int_gen, lst_gen, sim_num)
+  model <- "WFD"
+  smp_WFD <- cmpgenerateSampleTraj_2L(model, sel_cof, dom_par, rec_rat, pop_siz, int_frq, int_gen, lst_gen, sim_num, ptn_num[i])
+  grd_num <- 2 * pop_siz
+
+  dist[i, , ] <- cmpcalculateHellingerDist(smp_WFM, smp_WFD, grd_num)
+}
+
+
 ########################################################################################################################
 
 #' @section Section 4. Transition probability density approxiamtion
