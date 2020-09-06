d591211eed22f0824d848e13f4abdb921b70945c
zhangyi-he <jefferyhe@outlook.com>
2020-09-06 14:05:16 +0100

    Update Manuscript.R

---
+++ DiffusApprox/v1.0/Manuscript.R
@@ -0,0 +1,74 @@
+#' @title Numerical simulation of the two-locus Wright-Fisher diffusion with application to approximating transition probability densities
+#' @author Zhangyi He, Mark Beaumont and Feng Yu
+
+#' version 1.0
+
+# set the directory
+setwd("~/Documents/GitHub/WFM-2L-Approx")
+
+source("./DiffusApprox/v1.0/RFUN.R")
+
+#install.packages("RColorBrewer")
+library("RColorBrewer")
+
+#install.packages("viridis")
+library("viridis")
+
+# install.packages("pals")
+library("pals")
+
+#install.packages("ggplot2")
+library("ggplot2")
+
+#install.packages("plot3D")
+library("plot3D")
+
+#install.packages("MASS")
+library("MASS")
+
+#install.packages("coda")
+library("coda")
+
+#install.packages("Metrics")
+library("Metrics")
+
+setwd("~/Dropbox/Jeffery He/iResearch/Publications/2012/HE2020-WFM-2L-DiffusApprox-TheorPopulBiol")
+
+########################################################################################################################
+
+#' @section Section 1. Introduction
+
+########################################################################################################################
+
+#' @section Section 2. Wright-Fisher diffusion
+
+#' @section Section 2.1. Wright-Fisher diffusion
+
+#' @section Section 2.2. Diffusion approximation
+
+########################################################################################################################
+
+#' @section Section 3. Stochastic Taylor scheme
+
+#' @section Section 3.1. Equivalent SDE formulation
+
+#' @section Section 3.2. Performance evaluation
+
+########################################################################################################################
+
+#' @section Section 4. Transition probability density approxiamtion
+
+#' @section Section 3.1. Euler-Maruyama approximation
+
+#' @section Section 3.2. Monte Carlo integration
+
+#' @section Section 3.3. Monte Carlo integration with importance sampling
+
+#' @section Section 3.4. Performance evaluation
+
+########################################################################################################################
+
+#' @section Section 3. Discussion
+
+########################################################################################################################
+
